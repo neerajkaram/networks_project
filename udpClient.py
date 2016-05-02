0073942e6c63b316dd2f102f4781cc83c0076a50
Manali <manalilatkar@gmail.com>
2016-05-02 15:48:22 -0400

    added channel and improved window mechanism for Udep client servers

--- udpClient.py
+++ udpClient.py
@@ -1,35 +1,90 @@
 '''Author: Neeraj Date: 04/27/2016 ver:2.1 '''
-
+import json
 import socket
 
-#address ad port
+import time
+
+import select
+
+import CONSTANTS
+# address ad port
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5001
+UDP_CLIENT_PORT_1 = 5001
+UDP_CHANNEL_PORT = CONSTANTS.UDP_CHANNEL_PORT
+SENDING_TO_SERVER_PORT1 = 5000
+# creating UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(2)
 
-#creating UDP socket
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+# binding
+sock.bind((UDP_IP, UDP_CLIENT_PORT_1))
+# sock.timeo
+print 'I am client. My port is ', UDP_CLIENT_PORT_1
 
-#binding 
-sock.bind((UDP_IP, UDP_PORT))
+# take the number of msgs to send to server
+n = int(raw_input("enter number of msgs "))
+messages = {}
+for i in range(int(n)):
+    messages[i] = 'Hello World '+str(i)
+
+# take input for size of stop and wait window
+w = int(raw_input("Window size "))
+if n < w:
+    w=n
+window_starts = 0
+window_ends = w-1
+seq = 0
+acked_messages = []
+window = messages.keys()[0:w]
+while (messages):
+    # if len(messages) < w:
+    #     w = len(messages)
+    for i in window:
+        print window
+        msg = {'message': messages[i], 'port_to': SENDING_TO_SERVER_PORT1, 'port_from': UDP_CLIENT_PORT_1, 'seq_no': i}
+        print 'Sending '+ messages[i] + ' to ' +  str(SENDING_TO_SERVER_PORT1)
+        # sending to server port
+        sock.sendto(json.dumps(msg), (UDP_IP, CONSTANTS.UDP_CHANNEL_PORT))
 
 
-#take the number of msgs to send to server
-n=raw_input("enter number of msgs ")
+    # time.sleep(5)
+    input = [sock]
 
-#take input for size of stop and wait window
-w=raw_input("Window size ")
+    # inputready,outputready,exceptready = select.select(input,[],[])
+    # for s in inputready:
+    try:
+        for i in range(len(window)):
+            receivedbytes = sock.recv(1024)
+            ack_data = json.loads(receivedbytes)
+            print ack_data
+            acked_messages.append(ack_data['seq_no'])
+            window.remove(int(ack_data['seq_no']))
+            window_ends = window_ends+1
+            if window_ends<n:
+                window.append(window_ends)
+            messages.pop(ack_data['seq_no'])
+            print "Ack received for", ack_data['seq_no']
+    except Exception as e:
+        print e.message
 
-seq=0
-for i in range(0,int(n),1):
-    msg="Hello world "+str(i)
-    seq=seq+i
-    #sending to server port
-    sock.sendto(msg, (UDP_IP, 5000))
-    sock.sendto(str(seq), (UDP_IP, 5000))
-    data, addr = sock.recvfrom(1024)
-    print "Ack recived"+data
+    print 'Remaining messages\n'
+    print window
+    if len(window) is 0:
+        print 'last run'
+        print messages
+        break
 
-#closing the connection
+
+
+# seq = 0
+# for i in range(0, int(n), 1):
+#     seq = seq + i
+#     msg = {'message': "Hello world " + str(i), 'port_to':5000, 'port_from': 145, 'seq_no': seq+1}
+#     # sending to server port
+#     sock.sendto(json.dumps(msg), (UDP_IP, CONSTANTS.UDP_CHANNEL_PORT))
+#
+#     data, addr = sock.recvfrom(1024)
+#     print "Ack received" + data
+
+# closing the connection
 sock.close()
-
-
--- udpServer.py
+++ udpServer.py
@@ -1,32 +1,36 @@
 '''Author: Neeraj Date: 04/27/2016 ver:2.1 '''
-
+import json
 import socket
 
-#address,port of the server
+# address,port of the server
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5000
+UDP_SERVER_PORT = 5000
 
-#creating a UDP socket
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+print 'I am server. My port is ' ,UDP_SERVER_PORT
 
-#binding the address
-sock.bind((UDP_IP, UDP_PORT))
+# creating a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# binding the address
+sock.bind((UDP_IP, UDP_SERVER_PORT))
 
 print "Server listening"
 
 while True:
-	data, addr = sock.recvfrom(1024) # recving from client and buffer size is 1024 bytes
-	print "Receving message from:",str(addr)
-	print ""
-	print "received message:", data
-	seq, addr = sock.recvfrom(1024)
-	print "Seq no. is " +seq
-	print "Preparing to send ack: " + seq
-	#sending back the ack from where we were recving data
-	sock.sendto(str(seq),addr)
-	print "sent ack"
-	#print "address", addr
+    raw_data, addr = sock.recvfrom(1024)  # recving from client and buffer size is 1024 bytes
+    print "Receving message from:", str(addr)
+    print ""
+    print "received message:", raw_data
+    data = json.loads(raw_data)
+    print "Seq no. is " , data['seq_no']
+
+# uncomment below line when you want to test that server doesn't ack for message no. 3
+    # if data['seq_no'] != 3:
+
+    print "Preparing to send ack: "
+    # sending back the ack from where we were recving data
+    ack_message = {'message': 'ACK', 'port_to': data['port_from'], 'port_from': data['port_to'], 'seq_no': data['seq_no']}
+    sock.sendto(json.dumps(ack_message), addr)
+    print "sent ack"
+# print "address", addr
 sock.close()
-
-
